9481d84b713741d5156bb65468a8bc973777ed7e
HzBryan <HzBryan@users.noreply.github.com>
2018-02-11 17:23:23 -0500

    Login Validation

--- MyLands/MyLands/ViewModels/LoginViewModel.cs
+++ MyLands/MyLands/ViewModels/LoginViewModel.cs
@@ -1,9 +1,23 @@
 ﻿namespace MyLands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.ComponentModel;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
 
         #region Properties
         public string Email
@@ -14,14 +28,38 @@
 
         public string Password
         {
-            get;
-            set;
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if (this.password != value)
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+            }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+            }
         }
 
         public bool IsRemembered
@@ -29,30 +67,93 @@
             get;
             set;
         }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
+            }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
-
+            this.IsEnabled = true;
         }
         #endregion
 
         #region Commands
-        //public ICommand LoginCommand
-        //{
-        //    get
-        //    {
-        //        return new RelayCommand(Login)
-        //    }
-        //}
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
 
-        //public ICommand RegisterCommand
-        //{
-        //    get;
-        //    set;
-        //}
+        //Validacion del inicio de sesion
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;  //Habilitamos la accion de "Se esta ejecutando" para darle mayor visibilidad al usuario
+            this.IsEnabled = false; //Deshabilitamos los botones para que el usuario no pueda hacer nada
+
+            if (this.Email != "obrzz@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true; //Habilitamos los botones para el usuario
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+            
+            this.IsRunning = false;
+            this.IsEnabled = true; //Habilitamos los botones para el usuario
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Good one",
+                    "Accept");
+            return;
+        }
+
+        public ICommand RegisterCommand
+        {
+            get;
+            set;
+        }
         #endregion 
     }
 }
