29240e61ea3338954454ed20c22d8986a5e233c7
HzBryan <HzBryan@users.noreply.github.com>
2018-02-18 12:23:16 -0500

    Dab Change

--- MyLands/MyLands/ViewModels/LoginViewModel.cs
+++ MyLands/MyLands/ViewModels/LoginViewModel.cs
@@ -1,57 +1,27 @@
 ﻿namespace MyLands.ViewModels
 {
-<<<<<<< HEAD
-    using GalaSoft.MvvmLight.Command;
-=======
->>>>>>> parent of 9481d84... Login Validation
     using System.Windows.Input;
 
-<<<<<<< HEAD
-    public class LoginViewModel : BaseViewModel
-    {
-
-        #region Attributes
-        private string email;
-        private string password;
-        private bool isRunning;
-        private bool isEnabled;
-        #endregion
-
-=======
     public class LoginViewModel
     {
 
->>>>>>> parent of 9481d84... Login Validation
         #region Properties
-        //Inicialmente teniamos implementada la interfaz INotifyPropertyChanged aqui, pero para optimizar
-        //el codigo, creamos la BaseViewModel y heredamos la interfaz de ahi y hacemos la referencia propia
-        //para cada propiedad, para asi actualizar la vista
         public string Email
         {
-            get { return this.email; }
-            set { this.SetValue(ref this.email, value); }
+            get;
+            set;
         }
 
         public string Password
         {
-<<<<<<< HEAD
-            get { return this.password; }
-            set { this.SetValue(ref this.password, value); }
-=======
             get;
             set;
->>>>>>> parent of 9481d84... Login Validation
         }
 
         public bool IsRunning
         {
-<<<<<<< HEAD
-            get { return this.isRunning; }
-            set { this.SetValue(ref this.isRunning, value); }
-=======
             get;
             set;
->>>>>>> parent of 9481d84... Login Validation
         }
 
         public bool IsRemembered
@@ -59,15 +29,6 @@
             get;
             set;
         }
-<<<<<<< HEAD
-
-        public bool IsEnabled
-        {
-            get { return this.isEnabled; }
-            set { this.SetValue(ref this.isEnabled, value); }
-        }
-=======
->>>>>>> parent of 9481d84... Login Validation
         #endregion
 
         #region Constructors
