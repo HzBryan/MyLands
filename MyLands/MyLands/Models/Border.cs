ad4a39a1f76152b12f307c8e7ba8ab50a420d981
HzBryan <HzBryan@users.noreply.github.com>
2018-02-25 23:01:50 -0500

    Populating Tabs

---
+++ MyLands/MyLands/Models/Border.cs
@@ -0,0 +1,17 @@
+﻿namespace MyLands.Models
+{
+    public class Border
+    {
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
